a09573bfbb88b2831780d5ec31877b8f28ef1fb6
Leo0liveira <lfofreitas@gmail.com>
2026-02-04 14:20:03 -0300

    Concluindo o componente FeaturedPost

--- app/page.tsx
+++ app/page.tsx
@@ -13,18 +13,34 @@ export default async function HomePage() {
       <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
         <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
           <Image
-            className="group-hover:scale-105 transition"
-            src="/images/bryen_0.png"
+            className="w-full h-full object-cover object-center group-hover:scale-105 transition"
+            src="/images/news_01.png"
             width={1200}
             height={720}
             alt="Título do post"
+            priority
           />
         </Link>
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
-          dolorem est dolor porro, doloribus neque, quidem mollitia doloremque,
-          ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam harum blanditiis
-          mollitia?
+        <div className="flex flex-col gap-4 sm:justify-center">
+          <time
+            className="text-slate-600 block text-sm/tight"
+            dateTime="2025-04-20"
+          >
+            20/04/2025 10:00
+          </time>
+
+          <h1 className="text-2xl/tight font-extrabold sm:text-4xl">
+            <Link href="#">
+              Dia histórico para a ciência: revelada a primeira imagem de buraco
+              negro
+            </Link>
+          </h1>
+
+          <p>
+            Event Horizon Telescope, rede de oito observatórios ao redor do
+            mundo, divulgou hoje a primeira imagem real de um buraco negro
+            supermassivo, fenômeno previsto por Einstein
+          </p>
         </div>
       </section>
       <Suspense fallback={<SpinLoader />}>
