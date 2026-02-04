0b5be669911d945f4b364930b6ef480ad9894618
Leo0liveira <lfofreitas@gmail.com>
2026-02-04 09:01:21 -0300

    Server Components vs Client Components com 'use client'

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Header from "@/components/Header";
+
 export default function HomePage() {
-  return <h1>Olá de dentro de Page.tsx</h1>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
---
+++ components/Header/index.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Header() {
+  return (
+    <h1 className="text-6xl font-bold text-blue-500 hover:text-blue-50 hover:bg-blue-500 transition duration-1000">
+      Texto do meu H1
+    </h1>
+  );
+}
