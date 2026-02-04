563dc2c2d6afc901681800907fa47b62c7b56a0b
Leo0liveira <lfofreitas@gmail.com>
2026-02-04 09:32:15 -0300

    Criando um componente de SpinLoader (para loading)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/src/components/Header";
+import { SpinLoader } from "@/src/components/SpinLoader";
 
 export default function HomePage() {
   return (
     <div>
       <Header />
+      <SpinLoader />
     </div>
   );
 }
---
+++ src/components/SpinLoader/index.tsx
@@ -0,0 +1,7 @@
+export function SpinLoader() {
+  return (
+    <div className="flex items-center justify-center containerClasses">
+      <div className="w-10 h-10 border-5 border-t-transparent border-slate-900 rounded-full animate-spin"></div>
+    </div>
+  );
+}
