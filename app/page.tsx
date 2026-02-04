c05f1b64bdd0361202a860b7e0cb03196a5b6a88
Leo0liveira <lfofreitas@gmail.com>
2026-02-04 09:35:34 -0300

    Loading.tsx e Suspense para exibir fallback quando algo estiver carregando

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
-import Header from "@/src/components/Header";
+import { PostsList } from "@/src/components/PostList";
 import { SpinLoader } from "@/src/components/SpinLoader";
+import { Suspense } from "react";
 
-export default function HomePage() {
+export default async function HomePage() {
   return (
     <div>
-      <Header />
-      <SpinLoader />
+      <header>
+        <h1 className="text-6xl font-bold text-center py-8">Aqui é a HEADER</h1>
+      </header>
+
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+
+      <footer>
+        <p className="text-6xl font-bold text-center py-8">Footer</p>
+      </footer>
     </div>
   );
 }
