076d34982e591808a8538fbd261c2f50c4b13391
Leo0liveira <lfofreitas@gmail.com>
2026-02-04 21:10:41 -0300

    Sobre layout.tsx (root) e Metadata do Next para title e description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Container } from "../src/components/Container";
+import { Header } from "../src/components/Header";
 
 export const metadata: Metadata = {
-  title: "Galatic News",
+  title: {
+    default: "Galatic News",
+    template: "%s | Galatic News",
+  },
   description:
     "As últimas notícias do espaço, astronomia e exploração espacial.",
 };
@@ -14,7 +19,17 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        <Container>
+          <Header />
+
+          {children}
+
+          <footer>
+            <p className="text-6xl font-bold text-center py-8">Footer</p>
+          </footer>
+        </Container>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import { Container } from "@/src/components/Container";
-import { Header } from "@/src/components/Header";
 import { PostsList } from "@/src/components/PostList";
 import { SpinLoader } from "@/src/components/SpinLoader";
 import { Suspense } from "react";
@@ -7,17 +5,14 @@ import { PostFeatured } from "@/src/components/PostFeatured";
 
 export default async function HomePage() {
   return (
-    <Container>
-      <Header />
+    <>
       <Suspense fallback={<SpinLoader />}>
         <PostFeatured />
       </Suspense>
+
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
-      <footer>
-        <p className="text-6xl font-bold text-center py-8">Footer</p>
-      </footer>
-    </Container>
+    </>
   );
 }
