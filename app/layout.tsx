0c964704fe5458acf69329d4f7d6642d57e23c35
Leo0liveira <lfofreitas@gmail.com>
2026-02-04 21:15:32 -0300

    Separando o componente Footer (rodapé)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Container } from "../src/components/Container";
 import { Header } from "../src/components/Header";
+import { Footer } from "@/src/components/Footer";
 
 export const metadata: Metadata = {
   title: {
@@ -25,9 +26,7 @@ export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
 
           {children}
 
-          <footer>
-            <p className="text-6xl font-bold text-center py-8">Footer</p>
-          </footer>
+          <Footer />
         </Container>
       </body>
     </html>
