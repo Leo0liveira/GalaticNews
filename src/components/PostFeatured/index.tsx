f063b12fe6077d88db755319f9161d1c12f11b8a
Leo0liveira <lfofreitas@gmail.com>
2026-02-04 21:04:05 -0300

    React cache remove duplicação de chamadas da API ou Repositório

--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -1,9 +1,12 @@
 import { PostCoverImage } from "../PostCoverImage";
 import { PostSummary } from "../PostSummary";
+import { findAllPublicPosts } from "../../lib/post/queries";
 
-export function PostFeatured() {
-  const slug = "qualquer";
-  const postLink = `/post/${slug}`;
+export async function PostFeatured() {
+  const posts = await findAllPublicPosts();
+  const post = posts[0];
+
+  const postLink = `/post/${post.slug}`;
 
   return (
     <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
@@ -14,8 +17,8 @@ export function PostFeatured() {
         imageProps={{
           width: 1200,
           height: 720,
-          src: "/images/news_01.png",
-          alt: "Alt da imagem",
+          src: post.coverImageUrl,
+          alt: post.title,
           priority: true,
         }}
       />
@@ -23,13 +26,9 @@ export function PostFeatured() {
       <PostSummary
         postLink={postLink}
         postHeading="h1"
-        createdAt={"2019-04-10T00:24:38.616Z"}
-        excerpt={
-          "Event Horizon Telescope, rede de oito observatórios ao redor do mundo, divulgou hoje a primeira imagem real de um buraco negro supermassivo, fenômeno previsto por Einstein"
-        }
-        title={
-          "Dia histórico para a ciência: revelada a primeira imagem de buraco negro"
-        }
+        createdAt={post.createdAt}
+        excerpt={post.excerpt}
+        title={post.title}
       />
     </section>
   );
