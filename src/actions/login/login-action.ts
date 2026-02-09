414f79f9c1102d30c7677dc841a86784be26f74a
Leo0liveira <lfofreitas@gmail.com>
2026-02-09 19:53:49 -0300

    Trabalhando com cookies seguros no Next.js

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -1,7 +1,11 @@
-'use server';
+"use server";
 
-import { asyncDelay } from '../../utils/async-delay';
-import { verifyPassword } from '../../lib/login/manage-login';
+import { asyncDelay } from "../../utils/async-delay";
+import {
+  createLoginSession,
+  verifyPassword,
+} from "../../lib/login/manage-login";
+import { redirect } from "next/navigation";
 
 type LoginActionState = {
   username: string;
@@ -13,19 +17,19 @@ export async function loginAction(state: LoginActionState, formData: FormData) {
 
   if (!(formData instanceof FormData)) {
     return {
-      username: '',
-      error: 'Dados inválidos',
+      username: "",
+      error: "Dados inválidos",
     };
   }
 
   // Dados que o usuário digitou no form
-  const username = formData.get('username')?.toString().trim() || '';
-  const password = formData.get('password')?.toString().trim() || '';
+  const username = formData.get("username")?.toString().trim() || "";
+  const password = formData.get("password")?.toString().trim() || "";
 
   if (!username || !password) {
     return {
       username,
-      error: 'Digite o usuário e a senha',
+      error: "Digite o usuário e a senha",
     };
   }
 
@@ -33,22 +37,16 @@ export async function loginAction(state: LoginActionState, formData: FormData) {
   const isUsernameValid = username === process.env.LOGIN_USER;
   const isPasswordValid = await verifyPassword(
     password,
-    process.env.LOGIN_PASS || '',
+    process.env.LOGIN_PASS || "",
   );
 
   if (!isUsernameValid || !isPasswordValid) {
     return {
       username,
-      error: 'Usuário ou senha inválidos',
+      error: "Usuário ou senha inválidos",
     };
   }
 
-  // TODO: abaixo
-  // Aqui o usuário e senha são válidos
-  // Criar o cookie e redirecionar a página
-
-  return {
-    username,
-    error: 'USUÁRIO LOGADO COM SUCESSO!',
-  };
+  await createLoginSession(username);
+  redirect("/admin/post");
 }
