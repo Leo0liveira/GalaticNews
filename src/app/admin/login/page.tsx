58ec8a67096d38b0a8f0b1a1c672f82ce6fc8235
Leo0liveira <lfofreitas@gmail.com>
2026-02-09 19:12:11 -0300

    Componente LoginForm + server actions loginAction e logoutAction

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,5 +1,6 @@
+import { LoginForm } from "../../../components/admin/LoginForm";
 export const dynamic = "force-dynamic";
 
 export default async function AdminLoginPage() {
-  return <div className="py-16 text-6xl">AdminLoginPage</div>;
+  return <LoginForm />;
 }
