f9364182e263f6c00699eb22f7b21149eb171153
Leo0liveira <lfofreitas@gmail.com>
2026-02-09 13:46:30 -0300

    Validando o arquivo de imagem enviado ao servidor

--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -1,35 +1,74 @@
-"use server";
+"use client";
 
+import { uploadImageAction } from "../../../actions/upload/upload-image-action";
+import { Button } from "../../../components/Button";
 import { IMAGE_UPLOAD_MAX_SIZE } from "../../../lib/constants";
+import { ImageUpIcon } from "lucide-react";
+import { useRef, useTransition } from "react";
+import { toast } from "react-toastify";
 
-type UploadImageActionResult = {
-  url: string;
-  error: string;
-};
+export function ImageUploader() {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [isUploading, startTransition] = useTransition();
 
-export async function uploadImageAction(
-  formData: FormData,
-): Promise<UploadImageActionResult> {
-  const makeResult = ({ url = "", error = "" }) => ({ url, error });
+  function handleChooseFile() {
+    if (!fileInputRef.current) return;
 
-  if (!(formData instanceof FormData)) {
-    return makeResult({ error: "Dados inválidos" });
+    fileInputRef.current.click();
   }
 
-  const file = formData.get("file");
+  function handleChange() {
+    toast.dismiss();
 
-  if (!(file instanceof File)) {
-    return makeResult({ error: "Arquivo inválido" });
+    if (!fileInputRef.current) return;
+
+    const fileInput = fileInputRef.current;
+    const file = fileInput?.files?.[0];
+
+    if (!file) return;
+
+    if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
+      const readableMaxSize = IMAGE_UPLOAD_MAX_SIZE / 1024;
+      toast.error(`Imagem muito grande. Máx.: ${readableMaxSize}KB.`);
+
+      fileInput.value = "";
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    startTransition(async () => {
+      const result = await uploadImageAction(formData);
+
+      if (result.error) {
+        toast.error(result.error);
+        fileInput.value = "";
+        return;
+      }
+
+      // TODO: continuar depois
+      toast.success(result.url);
+    });
+
+    fileInput.value = "";
   }
 
-  if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
-    return makeResult({ error: "Arquivo muito grande" });
-  }
+  return (
+    <div className="flex flex-col gap-2 py-4">
+      <Button onClick={handleChooseFile} type="button" className="self-start">
+        <ImageUpIcon />
+        Enviar uma imagem
+      </Button>
 
-  if (!file.type.startsWith("image/")) {
-    return makeResult({ error: "Imagem inválida" });
-  }
-
-  // TODO: enviei o arquivo
-  return makeResult({ url: "URL" });
+      <input
+        onChange={handleChange}
+        ref={fileInputRef}
+        className="hidden"
+        name="file"
+        type="file"
+        accept="image/*"
+      />
+    </div>
+  );
 }
