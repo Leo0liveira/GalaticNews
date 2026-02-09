2c2ab8aaf15130ad77febcebd191937edc586632
Leo0liveira <lfofreitas@gmail.com>
2026-02-09 18:46:55 -0300

    Entendendo variáveis de ambiente e o .env.local

--- src/actions/upload/upload-image-action.ts
+++ src/actions/upload/upload-image-action.ts
@@ -1,12 +1,7 @@
-'use server';
+"use server";
 
-import {
-  IMAGE_SERVER_URL,
-  IMAGE_UPLOAD_DIRECTORY,
-  IMAGE_UPLOAD_MAX_SIZE,
-} from '../../lib/constants';
-import { mkdir, writeFile } from 'fs/promises';
-import { extname, resolve } from 'path';
+import { mkdir, writeFile } from "fs/promises";
+import { extname, resolve } from "path";
 
 type UploadImageActionResult = {
   url: string;
@@ -17,35 +12,34 @@ export async function uploadImageAction(
   formData: FormData,
 ): Promise<UploadImageActionResult> {
   // TODO: Verificar se o usuário está logado
-  
-  const makeResult = ({ url = '', error = '' }) => ({ url, error });
+
+  const makeResult = ({ url = "", error = "" }) => ({ url, error });
 
   if (!(formData instanceof FormData)) {
-    return makeResult({ error: 'Dados inválidos' });
+    return makeResult({ error: "Dados inválidos" });
   }
 
-  const file = formData.get('file');
+  const file = formData.get("file");
 
   if (!(file instanceof File)) {
-    return makeResult({ error: 'Arquivo inválido' });
+    return makeResult({ error: "Arquivo inválido" });
   }
 
-  if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
-    return makeResult({ error: 'Arquivo muito grande' });
+  const uploadMaxSize =
+    Number(process.env.NEXT_PUBLIC_IMAGE_UPLOAD_MAX_SIZE) || 921600;
+  if (file.size > uploadMaxSize) {
+    return makeResult({ error: "Arquivo muito grande" });
   }
 
-  if (!file.type.startsWith('image/')) {
-    return makeResult({ error: 'Imagem inválida' });
+  if (!file.type.startsWith("image/")) {
+    return makeResult({ error: "Imagem inválida" });
   }
 
   const imageExtension = extname(file.name);
   const uniqueImageName = `${Date.now()}${imageExtension}`;
 
-  const uploadFullPath = resolve(
-    process.cwd(),
-    'public',
-    IMAGE_UPLOAD_DIRECTORY,
-  );
+  const uploadDir = process.env.IMAGE_UPLOAD_DIRECTORY || "uploads";
+  const uploadFullPath = resolve(process.cwd(), "public", uploadDir);
   await mkdir(uploadFullPath, { recursive: true });
 
   const fileArrayBuffer = await file.arrayBuffer();
@@ -55,7 +49,9 @@ export async function uploadImageAction(
 
   await writeFile(fileFullPath, buffer);
 
-  const url = `${IMAGE_SERVER_URL}/${uniqueImageName}`;
+  const imgServerUrl =
+    process.env.IMAGE_SERVER_URL || "http://localhost:3000/uploads";
+  const url = `${imgServerUrl}/${uniqueImageName}`;
 
   return makeResult({ url });
 }
